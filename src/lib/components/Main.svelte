519f7b70d291abbac4a72dd13115a816f07ac3ca
tomashobzaids <thomashobza@ids.company>
2022-03-06 00:05:14 +0100

    add: routing to main

--- src/lib/components/Main.svelte
+++ src/lib/components/Main.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+import { goto } from "$app/navigation";
+
     import MainScreen from "$lib/components/MainScreen.svelte";
     import TodoDetail from "$lib/components/TodoDetail.svelte";
 
@@ -7,7 +9,10 @@
     // import { page } from '$app/stores';
 
     function handler(event) {
-        if (event.detail.direction === "right") selectedTodo.set(null);
+        if (event.detail.direction === "right") {
+            selectedTodo.set(null);
+            goto("/")
+        }
         if (event.detail.direction === "top") !todoOpen && openedDrawer.set(true); 
         if (event.detail.direction === "bottom") openedDrawer.set(false); 
     }
@@ -15,26 +20,27 @@
     let innerHeight: number;
     let innerWidth: number;
 
+
+    let todoOpen = false;
+    
     $: todoOpen = !!($selectedTodo);
 
-    // $: console.log($page.url.searchParams.get("id"));
-    // TODO getting new task from url
+    // $: console.log(todoOpen, $selectedTodo, "penis");
 </script>
 
 <svelte:window bind:innerHeight bind:innerWidth />
 
 <!-- overflow container -->
 <div class="w-full h-full overflow-hidden" use:swipe={{ timeframe: 300, minSwipeDistance: 100 }} on:swipe={handler}>
-    <div class="w-full h-full flex flex-row flex-nowrap transition-all duration-500 will-change-transform" class:open={todoOpen}>
+    <div class="w-full h-full flex flex-row flex-nowrap transition-all duration-500 will-change-transform {todoOpen && "open"}">
         <div
-            class="w-full h-full overflow-hidden transition-all duration-500 flex-shrink-0"
-            class:round={todoOpen}
+            class="w-full h-full overflow-hidden transition-all duration-500 flex-shrink-0 {todoOpen && "round"}"
             >
             <MainScreen {todoOpen} />
         </div>
 
         <!-- todo detail -->
-        <div class="w-[350px] h-full bg-white flex-shrink-0 transition-all">
+        <div class="w-[350px] h-full bg-white flex-shrink-0 transition-all overflow-hidden">
             <TodoDetail />
         </div>
     </div>
