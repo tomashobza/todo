41626eae326f6238cf3b6a30e090a89ff6abbb71
Tomáš Hobza <thomashobza@ids.company>
2022-03-10 13:17:13 +0100

    add: routing via url queries

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,22 @@
 <script>
     import Main from "$lib/components/Main.svelte";
-
     import { page } from '$app/stores'
-import { selectedTodo, todos } from "$ts/stores";
-    const email = $page.url.searchParams.get('id');
-    console.log(email);
+    import { modal, savedTodos, selectedTodo, todos } from "$ts/stores";
 
-    if (email) selectedTodo.set($todos[0]);
+    /** Tries to open the TODO via `id` from the URL query string. */
+    function openSelected(tId) {
+        let indexOfSelected = null;
+        indexOfSelected = $savedTodos.findIndex((elem) => elem == tId);
+
+        if (indexOfSelected < 0) {
+            modal.set(tId);
+        } else if (indexOfSelected >= 0) {
+            if ($todos[indexOfSelected]) selectedTodo.set($todos[indexOfSelected]);
+        }
+    }
+
+    const id = $page.url.searchParams.get('id');
+    $: if (id && $savedTodos) openSelected(id);
 </script>
 
 <Main />
