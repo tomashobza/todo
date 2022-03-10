f4bccf147138f4a45930adfcb1545f529592d0cf
Tomáš Hobza <thomashobza@ids.company>
2022-03-10 13:17:32 +0100

    add: TODO stores

--- src/ts/stores.ts
+++ src/ts/stores.ts
@@ -1,36 +1,37 @@
 import { derived, writable } from "svelte/store";
 
-export const todos = writable([]);
-todos.set([
-    {
-        id: "lulw",
-        done: false,
-        title: "Shopping list",
-        list: [
-            { title: "Do something...", done: true },
-            { title: "Do something...", done: false },
-            { title: "Do something...", done: true },
-        ],
-    },
-    {
-        id: "nevimxdd",
-        done: true,
-        title: "Homework",
-        list: [
-            { title: "Do something...", done: true },
-            { title: "Do something...", done: false },
-            { title: "Do something...", done: true },
-        ],
-    },
-]);
+/** Store that represents the `id`s of TODOs that are save in `localStorage` */
+export const savedTodos = writable([]);
 
-export const todosWithId = derived(todos, ($todos) =>
-    $todos.map((elem, id) => {
-        return { id, ...elem };
-    })
-);
+/** Store that represents data of stored TODOs that need to be fetched from firestore. */
+export const todos = derived(savedTodos, ($savedTodos) => {
+    // TODO change this to fetch data from firestore
 
+
+    // DEV
+    return $savedTodos.map(x => {
+        // !!! THIS OBJECT STRUCTURE NEEDS TO BE KEPT
+        return {
+            id: x,
+            done: false,
+            title: "Shopping list",
+            list: [
+                { title: "Do something...", done: true },
+                { title: "Do something...", done: false },
+                { title: "Do something...", done: true },
+            ],
+        };
+    });
+});
+
+/** Store that represents the data of currently open TODO */
 export const selectedTodo = writable(null);
+
+/** Store that represents the open/closed state of the drawer. */
 export const openedDrawer = writable(false);
 
+/** Store that represents an array of popups that are displayed.  */
 export const popups = writable([]);
+
+/** Store that represents the TODO ID of the currently prompted TODO.  */
+export const modal = writable(null);
